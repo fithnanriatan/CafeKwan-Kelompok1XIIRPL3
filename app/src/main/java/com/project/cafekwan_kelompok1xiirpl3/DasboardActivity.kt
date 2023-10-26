393e67bbd520fe433bfedc89bd14ff1d41f9ded5
iffahmufidah <iffamufidah1805>
2023-10-26 12:14:08 +0700

    menambahkan login

--- app/src/main/java/com/project/cafekwan_kelompok1xiirpl3/DasboardActivity.kt
+++ app/src/main/java/com/project/cafekwan_kelompok1xiirpl3/DasboardActivity.kt
@@ -1,11 +1,28 @@
 package com.project.cafekwan_kelompok1xiirpl3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.cafekwan_kelompok1xiirpl3.databinding.ActivityDasboardBinding
+import com.project.cafekwan_kelompok1xiirpl3.databinding.ActivityLoginBinding
 
 class DasboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDasboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dasboard)
+        binding = ActivityDasboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username= intent.getStringExtra("username").toString()
+        binding.txtWelcome.text="$username"
+        binding.navbarDasboard.setOnClickListener{
+            startActivity(Intent(this, DasboardActivity::class.java))
+        }
+        binding.navbarProduk.setOnClickListener{
+            startActivity(Intent(this, DasboardActivity::class.java))
+        }
+        binding.navbarPesanan.setOnClickListener{
+            startActivity(Intent(this, DasboardActivity::class.java))
+        }
     }
 }
