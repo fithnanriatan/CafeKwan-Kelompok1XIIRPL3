43ea33ed8a12b7f678a83baf1e242f5d30e6b69e
iffahmufidah <iffamufidah1805>
2023-11-01 07:30:55 +0700

    menambahkan update

--- app/src/main/java/com/project/cafekwan_kelompok1xiirpl3/pesanan.kt
+++ app/src/main/java/com/project/cafekwan_kelompok1xiirpl3/pesanan.kt
@@ -3,7 +3,7 @@ package com.project.cafekwan_kelompok1xiirpl3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class pesanan : AppCompatActivity() {
+class  pesanan : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pesanan)
--- app/src/main/java/com/project/cafekwan_kelompok1xiirpl3/update_menu.kt
+++ app/src/main/java/com/project/cafekwan_kelompok1xiirpl3/update_menu.kt
@@ -1,11 +1,50 @@
 package com.project.cafekwan_kelompok1xiirpl3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.project.cafekwan_kelompok1xiirpl3.databinding.ActivityUpdateMenuBinding
+import com.project.cafekwan_kelompok1xiirpl3.room.DB_CAFE
+import com.project.cafekwan_kelompok1xiirpl3.room.TB_MENU
 
 class update_menu : AppCompatActivity() {
+    private lateinit var binding: ActivityUpdateMenuBinding
+    private val db by lazy { DB_CAFE.getInstance(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update_menu)
+        binding = ActivityUpdateMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val id = intent.getStringExtra("idpesanan").toString().toInt()
+        val data = db.dao_cafe().getData(id)
+
+        binding.txtnamamenu.setText(data[0].kode_menu)
+        binding.txthargamenu.setText(data[0].harga_total.toString())
+        binding.txtstatusmenu.setText(data[0].status_pesanan)
+        binding.txtdeskrpsimenu.setText(data[0].)
+
+        binding.btnUbahProduk.setOnClickListener{
+            if (binding.txtnamamenu.text.isNotEmpty()&&
+                    binding.txthargamenu.text.isNotEmpty()&&
+                    binding.txtstatusmenu.text.isNotEmpty()&&
+                    binding.txtdeskrpsimenu.text.isNotEmpty()){
+
+                db.dao_cafe().UpdateData(TB_MENU(id,
+                binding.txtnamamenu.text.toString(),
+                binding.txthargamenu.text.toString().toInt(),
+                binding.txtstatusmenu.text.toString(),
+                binding.txthargamenu.text.toString().toInt()
+                ))
+
+                Toast.makeText(applicationContext,"Data berhasil diubah", Toast.LENGTH_SHORT).show()
+
+                startActivity(
+                    Intent(this,MainActivity::class.java))onBackPressed()
+            } else{
+                Toast.makeText(applicationContext, "ubah data terlebih dahulu", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
